b4e6bfbbbeff54e3bbe0af9d326cf54e3b194589
maxcodl <maxwantstohangout@gmail.com>
2026-06-11 17:37:45 +0530

    version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.auto.odo"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
     }
 
     // Load local.properties for local release builds safely
